d9bc3463d89bb03f7e46ebd8e7ea74a45b52c6e8
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2023-11-09 15:16:01 +0400

    Add project files.

---
+++ Garage_HomeWork_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_HomeWork_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st=new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            foreach (var item in st.Grades)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Garage_HomeWork_6/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_HomeWork_6
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        private int[] _grades;
+
+        public int[] Grades
+        {
+            get { return _grades; }
+            set
+            {
+                int count = 0;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                int[] tempArr=new int[count];
+                for (int i = 0, k = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        tempArr[k++] = value[i];
+                    }
+                }
+                _grades = tempArr;
+            }
+        }
+
+
+    }
+}
